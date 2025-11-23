ac90d39587a66bfc6759d7ea5af77b8add516c2c
Abdimajiid Ali <amajid0981@gmail.com>
2025-11-23 14:14:04 +0200

    fix: webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,61 @@
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const deps = require('./package.json').dependencies;
 
 const getPkgVersion = (pkgName) => {
   const pkgJsonPath = require.resolve(`${pkgName}/package.json`);
   return require(pkgJsonPath).version;
 };
 
-// common webpack utils
+// Common configuration
 const common = {
   devtool: false,
   entry: path.resolve(__dirname, './src/index.tsx'),
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      path: require.resolve('path-browserify'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-              plugins: ['@babel/transform-class-properties', 'babel-plugin-styled-components'],
+        use: {
+          loader: 'swc-loader',
+          options: {
+            jsc: {
+              parser: { syntax: 'typescript', tsx: true },
+              transform: { react: { runtime: 'automatic' } },
             },
           },
-        ],
+        },
       },
+
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: { maxSize: 10 * 1024 },
+        },
       },
       {
-        test: /\.(png|woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
       {
         oneOf: [
@@ -56,91 +67,134 @@ const common = {
           },
         ],
       },
-      { test: /\.txt$/, use: 'raw-loader' },
+      {
+        test: /\.txt$/,
+        use: 'raw-loader',
+      },
     ],
   },
   resolveLoader: {
     modules: [path.join(__dirname, './node_modules'), 'node_modules'],
   },
   plugins: [
-    new CompressionPlugin({
-      filename: '[path][base].br',
-      algorithm: 'brotliCompress',
-      test: /\.(js|css|html|svg)$/,
-      compressionOptions: {
-        level: 11,
+    new webpack.ProgressPlugin({
+      activeModules: false,
+      entries: true,
+      handler(percentage, message, ...args) {
+        const pct = Math.floor(percentage * 100);
+        console.log(`${pct}% : ${message}`, ...args);
       },
-      threshold: 10240,
-      minRatio: 0.8,
+      modules: false,
+      modulesCount: 100,
+      profile: false,
+      dependencies: false,
+      dependenciesCount: 100,
     }),
+    // new CompressionPlugin({
+    //   filename: '[path][base].br',
+    //   algorithm: 'brotliCompress',
+    //   test: /\.(js|css|html|svg)$/,
+    //   compressionOptions: { level: 11 },
+    //   threshold: 10240,
+    //   minRatio: 0.8,
+    // }),
+    // new CompressionPlugin({
+    //   filename: '[path][base].gz',
+    //   algorithm: 'gzip',
+    //   test: /\.(js|css|html|svg)$/,
+    //   threshold: 10240,
+    //   minRatio: 0.8,
+    // }),
+
     new webpack.ProvidePlugin({
       React: 'react',
-      '@feature-hub/react': '@feature-hub/react',
-      'styled-components': 'styled-components',
+      process: 'process/browser',
+    }),
+    new Dotenv({
+      path: './.env',
     }),
   ],
   stats: 'errors-only',
 };
 
-//   dev mode for development/demo
-const devConfig = () => {
-  return {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      hot: true,
-      open: true,
-      port: 3000,
-      historyApiFallback: {
-        disableDotRule: true, // allows paths with dots
-        rewrites: [{ from: /^\/.*$/, to: '/index.html' }],
-      },
-    },
-    output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, `./dist/dev`),
-      sourceMapFilename: '[name].[fullhash:8].map',
-      chunkFilename: '[id].[fullhash:8].js',
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, './public/index.html'),
-      }),
-      new ReactRefreshWebpackPlugin(),
-      new webpack.DefinePlugin({
-        'process.env.name': JSON.stringify('Vishwas'),
-      }),
-      new Dotenv({
-        path: './.env.dev',
-      }),
-    ],
-  };
-};
-
-//   build modern feature app to be integrated into featureHub container moduleType AMD
-const buildToHostAsFeatureApp = (env) => ({
-  mode: 'production',
-  entry: path.join(__dirname, './src/app/AppDefinition.tsx'),
-  externals: {
-    react: 'react',
-    '@feature-hub/react': '@feature-hub/react',
+// Configuration for development
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000,
+    historyApiFallback: true,
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, `./dist/fa-main`),
+    path: path.resolve(__dirname, `./dist/dev`),
     sourceMapFilename: '[name].[fullhash:8].map',
     chunkFilename: '[id].[fullhash:8].js',
-    libraryTarget: 'umd',
+    clean: true,
+    publicPath: '/',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      publicPath: '/',
+    }),
+
+    new Dotenv({
+      path: `./.env.dev`,
+    }),
+
+    new ReactRefreshWebpackPlugin(),
+
+  ],
+};
+
+// Common production settings
+const createProdConfig = (env) => ({
+  mode: 'production',
+  entry: {
+    main: path.join(__dirname, './src/index.tsx'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, `./dist`),
+    sourceMapFilename: '[name].[fullhash:8].map',
+    chunkFilename: '[id].[fullhash:8].js',
     clean: true,
   },
+  cache: {
+    type: 'filesystem',
+    compression: 'gzip',
+    buildDependencies: {
+      config: [__filename],
+    },
+  },
   plugins: [
-    new ReactRefreshWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      publicPath: '/',
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public'),
+          to: path.resolve(__dirname, 'dist'),
+          globOptions: {
+            ignore: ['**/index.html'], // since HtmlWebpackPlugin already handles it
+          },
+        },
+      ],
+    }),
     new webpack.container.ModuleFederationPlugin({
       shared: {
-        react: { singleton: true, eager: true },
-        'react-dom': { singleton: true, eager: true },
+        react: { singleton: true, eager: true, requiredVersion: deps['react'] },
       },
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+      '@feature-hub/react': '@feature-hub/react',
+      'styled-components': 'styled-components',
     }),
     new webpack.DefinePlugin({
       'process.env.REACT_VERSION': JSON.stringify(getPkgVersion('react')),
@@ -149,67 +203,46 @@ const buildToHostAsFeatureApp = (env) => ({
     new Dotenv({
       path: `./.env.${env}`,
     }),
+    new Dotenv({
+      path: `./.env`,
+    }),
   ],
 });
 
-//   Build modern feature app entry index to be hosted as an application
-const buildToHostAsStaticWebApp = (env) => {
-  return {
-    mode: 'production',
-    entry: {
-      main: path.join(__dirname, './src/index.tsx'),
-    },
-    output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, `./dist`),
-      sourceMapFilename: '[name].[fullhash:8].map',
-      chunkFilename: '[id].[fullhash:8].js',
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, './public/index.html'),
-      }),
-      new Dotenv({
-        path: `./.env.${env}`,
+// Common optimization settings
+const optimizationConfigs = {
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        minify: TerserPlugin.swcMinify,
+        parallel: true,
+        terserOptions: { compress: true },
       }),
     ],
-  };
-};
-
-const optimiseForStaticWebApp = {
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
     splitChunks: {
       chunks: 'all',
+      maxInitialRequests: 10,
+      minSize: 20000,
       cacheGroups: {
-        vendor: {
+        vendors: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          priority: -10,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
         },
       },
     },
-    runtimeChunk: 'single',
   },
 };
 
+// Export configuration based on environment
 module.exports = (envVars) => {
   const { env } = envVars;
-  //   switch method to switch between builds
+
   switch (env) {
     case 'dev':
-      return [
-        merge(common, buildToHostAsFeatureApp('dev')),
-        merge(common, optimiseForStaticWebApp, buildToHostAsStaticWebApp('dev')),
-      ];
+      return merge(common, optimizationConfigs, createProdConfig('dev'));
     default:
-      return merge(common, optimiseForStaticWebApp, devConfig());
+      return merge(common, devConfig);
   }
 };
+
