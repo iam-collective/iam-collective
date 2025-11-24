9ad59fdf4d5f6d68ce805d2b9657020348088bf3
khumo-mogotlhe_mtn <Khumo.Mogotlhe@mtn.com>
2025-11-24 21:35:26 +0200

    fix(layout): updated styles

--- src/app/Global.styles.tsx
+++ src/app/Global.styles.tsx
@@ -1,16 +1,37 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-:root {
+  /* Lora Local Fonts */
+  @font-face {
+    font-family: 'Lora';
+    src: url('./assets/fonts/Lora-Regular.ttf') format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Lora';
+    src: url('./assets/fonts/Lora-Medium.ttf') format('truetype');
+    font-weight: 500;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Lora';
+    src: url('./assets/fonts/Lora-Bold.ttf') format('truetype');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  :root {
     --primary-colour: #d31875;
     --background-color: #f7fafc;
     --primary-text-colour: #2d3748;
     --danger-color: #e53e3e;
   }
-   body{
+
+  body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Work Sans', sans-serif;
-   } 
+    font-family: 'Lora', serif;
+  }
 `;
